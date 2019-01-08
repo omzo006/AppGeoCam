da0ffae623294e31d773cf3596a157503f77ad06
omar <omar.m@codeur.online>
2019-01-08 12:54:22 +0000

    1er commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+declare let L;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    const map = L.map('map').setView([14.4137, -17.2638], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.Routing.control({
+      waypoints: [
+        L.latLng(57.74, 11.94),
+        L.latLng(57.6792, 11.949)
+      ]
+    }).addTo(map);
+  }
+
+}
