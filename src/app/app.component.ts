d7a110ebb9d75a5cdb799ec013b5d6bd3c44c366
omar <omar.m@codeur.online>
2019-01-09 10:56:15 +0000

    2eme commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,27 @@ declare let L;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Implementation de OnInit
 export class AppComponent implements OnInit {
 
   constructor() {
 
   }
-
+// Fonction d'initialisation du composant.
   ngOnInit() {
-    const map = L.map('map').setView([14.4137, -17.2638], 13);
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
 
+    const map = L.map('map').setView([14.6937000, -17.5440600], 12);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+   // const marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+    const myIcon = L.icon({
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    });
+    L.marker([14.69686602, -17.47185787], {icon: myIcon}).bindPopup('Je suis un camera de surveillance ').addTo(map).openPopup();
+
 
     L.Routing.control({
       waypoints: [
